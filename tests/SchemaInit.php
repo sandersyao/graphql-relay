b58131d3308bce5131c19187ed0e4cf8a985257f
pikaqiu <pikaqiu@kdd.com>
2019-06-13 09:53:10 +0800

    增加测试用例注释

--- tests/SchemaInit.php
+++ tests/SchemaInit.php
@@ -7,10 +7,24 @@ use GraphQL\Type\Schema;
 use GraphQLRelay\Tests\Sim\Query;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * 测试用例初始化
+ *
+ * Class SchemaInit
+ * @package GraphQLRelay\Tests
+ */
 class SchemaInit extends TestCase
 {
+    /**
+     * 基境schema
+     *
+     * @var
+     */
     protected $schema;
 
+    /**
+     * 测试基境建立
+     */
     public function setUp()
     {
         parent::setUp();
